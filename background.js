a32f24a17070a5883719c64d81d10d2be815f607
Mike Sabo <msabo@dolbey.com>
2014-03-15 15:40:54 -0400

    Changed the way the program worked. All code was updated to reflect better javascript methods. Fixed a bug where the user's word list from google might not return before the program added it to the list of words to be searched for.

--- background.js
+++ background.js
@@ -1,13 +1,16 @@
-//      Notification options
-var opt = {
-  type: "basic",
-  title: "Bleep It!",
-  message: "Your word has been successfully added!",
-  iconUrl: "toast.png"
-};
+//If first launch, set option to False.
+chrome.storage.sync.get('enableWords', function (items) {
+    var enableWordsResult = items.enableWords;
 
-//      Bleep word will add word to sync storage
-var bleepWord = function(info) {
+    if (typeof enableWordsResult === 'undefined') {
+        chrome.storage.sync.set({
+            'enableWords': 'False'
+        }, function () {});
+    }
+});
+
+//Bleep Word - this will add highlighted word to sync storage
+var bleepWord = function (info) {
     //chrome.storage.sync.clear();
     
     chrome.storage.sync.get('userAddedWords', function (userWords) {
@@ -27,17 +30,15 @@ var bleepWord = function(info) {
     });
 };
 
-//		If first launch, set option to False.
-chrome.storage.sync.get('enableWords', function(items) {
-    enableWordsResult = items.enableWords;
-    
-    if (typeof enableWordsResult === 'undefined') {
-		  chrome.storage.sync.set({'enableWords': 'False'}, function () {
-    	});
-    }
-});
+//Notification options
+var opt = {
+    type: "basic",
+    title: "Bleep It!",
+    message: "Your word has been successfully added!",
+    iconUrl: "toast.png"
+};
 
-//      Creates right-click menu item
+//Creates right-click menu item
 var menu = chrome.contextMenus.create({
     "title": "Bleep Word!",
     "contexts": ["selection"],
